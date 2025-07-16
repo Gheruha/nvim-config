4a8648889faae844ad8a23ed8723f7bb9d57327a
Gheruha <maxgheruha@gmail.com>
2025-07-16 17:08:02 +0300

    Added neotree & separate plugins file.

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-                        
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -24,42 +24,11 @@ vim.opt.rtp:prepend(lazypath)
 -- Leader keys
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-																								
 
 -- Setup lazy.nvim with eager-loading for frequently used plugins
-require("lazy").setup({
-  spec = {
-    -- Theme plugin (loads at startup)
-    {
-      "oxfist/night-owl.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        require("night-owl").setup()
-        vim.cmd.colorscheme("night-owl")
-      end,
-    },
-    -- Telescope (always loaded at startup)
-    {
-      "nvim-telescope/telescope.nvim",
-      tag = "0.1.8",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      lazy = false,
-      config = function()
-        require("telescope").setup({})
-      end,
-    },
-    -- Tree-sitter (always loaded at startup)
-    {
-      "nvim-treesitter/nvim-treesitter",
-      branch = "master",
-      lazy = false,
-      build = ":TSUpdate",
-    },
-  },
-
-  install = { colorscheme = { "night-owl" } },
-  checker = { enabled = true },
+require("lazy").setup("plugins", {
+   install = { colorscheme = { "night-owl" } },
+   checker = { enabled = true },
 })
 
 -- Telescope key mappings (available immediately)
@@ -67,10 +36,6 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<D-f>", builtin.find_files, { desc = "Telescope Find Files" })
 vim.keymap.set("n", "<D-g>", builtin.live_grep,  { desc = "Telescope Live Grep"  })
 
--- Tree-sitter
-local config = require("nvim-treesitter.configs")
-config.setup({
-	ensure_installed = {"lua", "javascript", "typescript", "cpp", "c", "sql", "python", "java", "css", "html"},
-	highlight = { enable = true },
-        indent = { enable = true },
-})
+-- Neotree key mappings (available immediately)
+vim.keymap.set("n", "<D-b>", ":Neotree toggle position=right<CR>", {})
+
